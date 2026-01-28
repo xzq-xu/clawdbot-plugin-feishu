5f8ab88b474bdf032810c94e93d998fa4a4ff63a
samzong <samzong.lu@gmail.com>
2026-01-28 18:50:21 +0800

    update workflow
    Signed-off-by: samzong <samzong.lu@gmail.com>

--- scripts/sync-version.js
+++ scripts/sync-version.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /**
- * Sync version from package.json to moltbot.plugin.json
+ * Sync version from package.json to manifest files
  */
 import { readFileSync, writeFileSync } from 'fs';
 import { fileURLToPath } from 'url';
@@ -10,10 +10,13 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const root = join(__dirname, '..');
 
 const pkg = JSON.parse(readFileSync(join(root, 'package.json'), 'utf8'));
-const manifestPath = join(root, 'moltbot.plugin.json');
-const manifest = JSON.parse(readFileSync(manifestPath, 'utf8'));
 
-manifest.version = pkg.version;
-writeFileSync(manifestPath, JSON.stringify(manifest, null, 4) + '\n');
+// Sync both manifest files
+for (const name of ['moltbot.plugin.json', 'clawdbot.plugin.json']) {
+    const path = join(root, name);
+    const manifest = JSON.parse(readFileSync(path, 'utf8'));
+    manifest.version = pkg.version;
+    writeFileSync(path, JSON.stringify(manifest, null, 4) + '\n');
+}
 
 console.log(`Synced version to ${pkg.version}`);
